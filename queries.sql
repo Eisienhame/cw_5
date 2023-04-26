50e16ab051a5fa86612ea5350868ed12f62fd35b
Eisiehame <knurov24@gmail.com>
2023-04-26 22:20:14 +0200

    обе табл заполнятся

--- queries.sql
+++ queries.sql
@@ -4,13 +4,11 @@ CREATE TABLE vacancies
 	vac_name varchar NOT NULL,
 	salary int,
 	id_emp int NOT NULL,
-	emp_name varchar NOT NULL,
 	link_vac varchar UNIQUE
 )
 
 CREATE TABLE employeers
 (
 	id_emp int PRIMARY KEY,
-	emp_name varchar UNIQUE,
-	vacancies int
+	emp_name varchar UNIQUE
 )
--- utils/for_bd.py
+++ utils/for_bd.py
@@ -9,11 +9,15 @@ def get_vacancies_in_table(id):
             password="12345678") as conn:
         with conn.cursor() as cur:
             for row in data_for_bd:
-                cur.execute('INSERT INTO vacancies(id_vac, vac_name, salary, id_emp, emp_name, link_vac)'
-                            'VALUES(%s, %s, %s, %s, %s, %s)', (row["id_vac"], row["name"], row["salary"], row["id_emp"], row["emp_name"], row["url"]))
+                cur.execute('INSERT INTO vacancies(id_vac, vac_name, salary, id_emp, link_vac)'
+                            'VALUES(%s, %s, %s, %s, %s)', (row["id_vac"], row["name"], row["salary"], row["id_emp"], row["url"]))
+                e_id = row["id_emp"]
+                e_name = row["emp_name"]
+            cur.execute('INSERT INTO employeers(id_emp, emp_name)'
+                        'VALUES(%s, %s)',
+                        (e_id, e_name))
 
-
-get_vacancies_in_table(869045)
+get_vacancies_in_table(640251)
 # print(get_udated_list(869045))
 # for i in get_udated_list(869045):
 #     print(i)

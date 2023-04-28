40d28def1e5ac59366f0a083cbad7ed299f4c832
Eisiehame <knurov24@gmail.com>
2023-04-28 18:19:22 +0200

    dbmanager 1 функция

--- classes/dbmanager.py
+++ classes/dbmanager.py
@@ -2,12 +2,13 @@ from abc import abstractmethod
 import psycopg2
 class DBManager():
 
-    def __init__(self):
-
-        pass
+    # def __init__(self):
+    #
+    #     pass
 
     @abstractmethod
     def connection(self, rules:str):
+        'Подкл к бд и возвращает инфу по sql запросу'
         with psycopg2.connect(
                 host="localhost",
                 database="cw_5_hh",
@@ -17,24 +18,30 @@ class DBManager():
                 cur.execute(rules)
                 records = cur.fetchall()
             return records
-    def get_companies_and_vacancies_count():
+    def get_companies_and_vacancies_count(self):
         'получает список всех компаний и количество вакансий у каждой компании.'
-        pass
+        req = 'SELECT emp_name, COUNT(*) as total_vacancies FROM vacancies LEFT JOIN employeers USING(id_emp) GROUP BY emp_name ORDER BY total_vacancies DESC, emp_name'
+        data = self.connection(req)
+        for i in data:
+            print(f'Название комании: {i[0]} \n Кол-во вакансий: {i[1]}')
+            print('__________________________________________________')
 
-    def get_all_vacancies():
+
+    def get_all_vacancies(self):
         '''получает список всех вакансий с указанием названия компании,
          названия вакансии и зарплаты и ссылки на вакансию'''
         pass
-    def get_avg_salary():
+    def get_avg_salary(self):
         'получает среднюю зарплату по вакансиям'
         pass
 
-    def get_vacancies_with_higher_salary():
+    def get_vacancies_with_higher_salary(self):
         'получает список всех вакансий, у которых зарплата выше средней по всем вакансиям'
         pass
 
-    def get_vacancies_with_keyword():
+    def get_vacancies_with_keyword(self):
         'получает список всех вакансий, в названии которых содержатся переданные в метод слова, например “python”'
 
 x = DBManager()
-print(x.connection('SELECT * FROM employeers'))
+#print(x.connection('SELECT * FROM employeers'))
+x.get_companies_and_vacancies_count()

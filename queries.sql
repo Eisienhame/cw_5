7d5a4edccf922b01cab85de6f2109f5459ee4f9d
Eisiehame <knurov24@gmail.com>
2023-04-28 17:24:33 +0200

    sql запросы

--- queries.sql
+++ queries.sql
@@ -1,3 +1,5 @@
+'Создание таблиц'
+
 CREATE TABLE vacancies
 (
 	id_vac int PRIMARY KEY,
@@ -12,3 +14,39 @@ CREATE TABLE employeers
 	id_emp int PRIMARY KEY,
 	emp_name varchar UNIQUE
 )
+
+'список всех компаний и количество вакансий у каждой компании'
+
+SELECT emp_name, COUNT(*) as total_vacancies
+FROM vacancies
+LEFT JOIN employeers USING(id_emp)
+GROUP BY emp_name
+ORDER BY total_vacancies DESC, emp_name
+
+'получает список всех вакансий с указанием названия компании,названия вакансии и зарплаты и ссылки на вакансию'
+
+SELECT employeers.emp_name, vac_name, salary, link_vac
+FROM vacancies
+INNER JOIN employeers USING(id_emp)
+GROUP BY salary, link_vac, employeers.emp_name, vac_name
+ORDER BY employeers.emp_name, salary DESC
+
+'получает среднюю зарплату по вакансиям, 0 не берем т.к. там зп неизвестна'
+
+SELECT ROUND(AVG(salary)) as avg_salary
+FROM vacancies
+WHERE salary > 0
+
+'получает список всех вакансий, у которых зарплата выше средней по всем вакансиям'
+
+SELECT vac_name, salary, link_vac
+FROM vacancies
+WHERE salary > (SELECT AVG(salary) FROM vacancies WHERE salary > 0)
+ORDER BY salary DESC, vac_name
+
+'получает список всех вакансий, в названии которых содержатся переданные в метод слова, например “python”'
+
+SELECT *
+FROM vacancies
+WHERE vac_name ILIKE '%python%'
+ORDER BY salary DESC
